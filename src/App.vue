d6d36bb7ff0834d2b74984a7724fa4fc7cd23031
Alex <xinfoz19@gmail.com>
2025-11-06 18:13:53 +0400

    refactor: Extracted calendar logic into CalendarView component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,10 @@
 <script setup>
-import { ref } from 'vue'
-import AppCalendar from '@/components/AppCalendar.vue'
-import LocaleSwitcher from '@/components/LocaleSwitcher.vue'
-
-const selectedDate = ref(null)
-const myInitialDate = '' // Если дата не указана, будет использоваться текущая дата
-const locale = ref('ru')
-
-const handleDateSelected = (date) => {
-  selectedDate.value = date
-}
+import CalendarView from '@/views/CalendarView.vue'
 </script>
 
 <template>
   <main>
-    <LocaleSwitcher v-model="locale" />
-
-    <AppCalendar
-      :initial-date="myInitialDate"
-      :locale="locale"
-      @date-selected="handleDateSelected"
-    />
-    
-    <div v-if="selectedDate" class="selected-date-info">
-      Выбранная дата: {{ selectedDate.toLocaleDateString() }}
-    </div>
+    <CalendarView />
   </main>
 </template>
 
@@ -32,9 +12,5 @@ const handleDateSelected = (date) => {
 main {
   padding: 20px;
   text-align: center;
-}
-.selected-date-info {
-  margin-top: 20px;
-  font-size: 1.2em;
 }
 </style>
---
+++ src/views/CalendarView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import AppCalendar from '@/components/AppCalendar.vue'
+import LocaleSwitcher from '@/components/LocaleSwitcher.vue'
+
+const selectedDate = ref(null)
+const myInitialDate = '' // Если дата не указана, будет использоваться текущая дата
+const locale = ref('ru')
+
+const handleDateSelected = (date) => {
+  selectedDate.value = date
+  console.log('Выбранная дата:', date)
+}
+</script>
+
+<template>
+  <div>
+    <LocaleSwitcher v-model="locale" />
+
+    <AppCalendar
+      :initial-date="myInitialDate"
+      :locale="locale"
+      @date-selected="handleDateSelected"
+    />
+  </div>
+</template>
+
+<style scoped>
+</style>
